e356bc52b51a7eaec6c3a99e6d659f948177be63
Graham Lalor <grahamlalor@gmail.com>
2017-11-21 22:20:21 +0000

    added experience and 2 more paralax images

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 
-$(document).ready(function() {
-        $(document).on('click', '.showMore', function () {
-            $('.showMore').text("Show less");
-            $('.showMore').attr('class', 'showLess btn btn-info');
-            $('.skills').animate({height: '1500px'}, "slow");
-        });
+$(document).ready(function () {
+    $(document).on('click', '.showMore', function () {
+        $('.showMore').text("Show less");
+        $('.showMore').attr('class', 'showLess btn btn-info');
+        $('.skills').animate({ height: '1500px' }, "slow");
+    });
 
-        $(document).on('click', '.showLess', function () {
-            //$('.moreSkills').css("visibility", "hidden");
-            $('.showLess').text("Show more");
-            $('.showLess').attr('class', 'showMore btn btn-info');
-            $('.skills').animate({height: '420px'}, "slow");
-        });
+    $(document).on('click', '.showLess', function () {
+        //$('.moreSkills').css("visibility", "hidden");
+        $('.showLess').text("Show more");
+        $('.showLess').attr('class', 'showMore btn btn-info');
+        $('.skills').animate({ height: '420px' }, "slow");
+    });
 
-        $(".anchor").click(function(e) {
-            e.preventDefault();
-            anchor = $(this).attr('href');
-            $("html, body").animate({
-              'scrollTop':   $(anchor).offset().top
-            }, 1000);
-        });
+    $("#sendEmail").click(function () {
+        $('#contactModal').modal('toggle');
+        $('#light').delay(2500).queue(function (next) {$(this).css('display','none'); next();});
+        $('#fade').delay(2500).queue(function (next) { $(this).css('display','none'); next();});
+    });
 
-        $(".navbar-nav li").on("click", function(){
-            $(".navbar-nav").find(".active").removeClass("active");
-            $(this).parent().addClass("active");
-         });
+    $(".anchor").click(function (e) {
+        e.preventDefault();
+        anchor = $(this).attr('href');
+        $("html, body").animate({
+            'scrollTop': $(anchor).offset().top
+        }, 1000);
+    });
+
+    $(".navbar-nav li").on("click", function () {
+        $(".navbar-nav").find(".active").removeClass("active");
+        $(this).parent().addClass("active");
+    });
 
 });
